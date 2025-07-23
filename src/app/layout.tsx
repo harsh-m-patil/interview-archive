0f4f56e511ed422d0e93d9f4ad6c443d68a977c8
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-07-23 19:37:34 +0530

    feat: add analytics with vercel/anayltics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ReactQueryClientProvider } from "@/components/providers/query-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
@@ -44,6 +45,7 @@ export default function RootLayout({
           </ReactQueryClientProvider>
           <Toaster richColors position="top-center" />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
