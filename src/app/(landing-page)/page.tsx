24cc4c1bd1008239316319afd14ee070698acb4d
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-08-17 15:00:28 +0530

    feat: add new landing page

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,3 +1,4 @@
+import BentoGrid from "./_components/bento";
 import { Hero } from "./_components/hero";
 
 export default function Home() {
@@ -6,6 +7,10 @@ export default function Home() {
       <div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <Hero />
+          <BentoGrid />
+          <footer className="mt-16 text-center text-sm text-muted-foreground py-8">
+            <p>Made with ❤️ by Harsh</p>
+          </footer>
         </div>
       </div>
     </>
